6bcea05a058b8f1c7769a32f05f16a6821fa7273
vadym <kovalchuk.vadim@gmail.com>
2021-03-08 00:19:10 +0200

    settings and compose

---
+++ settings.py
@@ -0,0 +1,21 @@
+import os
+
+import yaml
+
+from common.logging_tools import get_datetime_stamp
+
+with open("settings.yaml", 'r') as fh:
+    try:
+        yaml_dict = yaml.safe_load(fh)
+    except yaml.YAMLError as exc:
+        raise SystemError('YAML settings load failed') from exc
+
+
+instance_type = os.getenv('INSTANCE_TYPE', 'docker')
+env_db_settings = os.getenv('DB_HOST', '127.0.0.1')
+
+db_host = 'database' if instance_type == 'docker' else env_db_settings
+db_creds = yaml_dict['database']
+log_path = f'log/django/log_{get_datetime_stamp()}.txt' if instance_type == 'docker' else f'log_{get_datetime_stamp()}.txt'
+
+secret_key = yaml_dict['django']['secret_key']
