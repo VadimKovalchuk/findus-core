5c08449400aee2352f3bba2faf534faea63157e4
Vadym Kovalchuk <kovalchuk.vadim@gmail.com>
2023-04-17 22:42:06 +0300

    test base norm covered

--- tests/test_edge/test_normalization.py
+++ tests/test_edge/test_normalization.py
@@ -16,8 +16,13 @@ logger = logging.getLogger(__name__)
 MINMAX_DATA = {str(idx): idx * 2 for idx in range(0, 11)}
 
 
-
-def test_minmax(client_on_dispatcher: Client):
+@pytest.mark.parametrize("normalization_method", [
+        "minmax",
+        "minmax_inverted",
+        "z_score",
+        "robust"
+])
+def test_normalization_parameters(client_on_dispatcher: Client, normalization_method: str):
     client = client_on_dispatcher
     test_task = deepcopy(task_body)
     test_task['client'] = client.broker.queue
@@ -25,17 +30,11 @@ def test_minmax(client_on_dispatcher: Client):
     test_task['function'] = 'normalization'
     args = {
         "input_data": MINMAX_DATA,
-        "norm_method": "minmax"
+        "norm_method": normalization_method
     }
     test_task['arguments'] = args
     client.broker.publish(test_task)
     # Validating result on client
     _, result = next(client.broker.pull())
-    data = json.loads(result['result'])
-    logger.info(json.dumps(data, indent=4))
-    price_count = len(data['prices'])
-    # dividend_count = len(data['dividends'])
-    # logger.info(f'Price rows: {price_count}, Dividend rows: {dividend_count}')
-    # _min, _max = calculate_boundaries(62, 5)
-    # assert _min <= price_count <= _max, f'Price count does not fit expected boundaries({_min},{_max})'
-    # assert dividend_count, 'Dividend rows are missing'
+    result = json.loads(result['result'])
+    logger.info(json.dumps(result, indent=4))
