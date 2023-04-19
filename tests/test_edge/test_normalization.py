3b3adda578889219223d1a7bf32923a0a1a1d742
Vadym Kovalchuk <kovalchuk.vadim@gmail.com>
2023-04-19 23:33:13 +0300

    all base norm methods validated

--- tests/test_edge/test_normalization.py
+++ tests/test_edge/test_normalization.py
@@ -13,23 +13,52 @@ from dcn.common.data_structures import task_body
 
 logger = logging.getLogger(__name__)
 
-MINMAX_DATA = {str(idx): idx * 2 for idx in range(0, 11)}
+UNIFORM_DISTRIBUTION_DATA = {str(idx): idx * 2 for idx in range(0, 11)}
+NORMAL_DISTRIBUTION_DATA = {"1": 1, "2": 3, "3": 4, "4": 4, "5": 5, "6": 5, "7": 6, "8": 6, "9": 7, "10": 9}
+# UNIFORM_DATA_WITH_OUTLIER = {"11": 21}
+# UNIFORM_DATA_WITH_OUTLIER.update(UNIFORM_DISTRIBUTION_DATA)
 
 
-@pytest.mark.parametrize("normalization_method", [
-        "minmax",
-        "minmax_inverted",
-        "z_score",
-        "robust"
+@pytest.mark.parametrize("normalization_method, data, expected", [
+        pytest.param(
+            "minmax",
+            UNIFORM_DISTRIBUTION_DATA,
+            {"min": 0.0, "max": 20.0},
+            id="minmax"
+        ),
+        pytest.param(
+            "minmax_inverted",
+            UNIFORM_DISTRIBUTION_DATA,
+            {"min": 0.0, "max": 20.0},
+            id="minmax_inverted"
+        ),
+        pytest.param(
+            "z_score",
+            NORMAL_DISTRIBUTION_DATA,
+            {"mean": 5.0, "std": 2.0976176963403033},
+            id="z_score"
+        ),
+        pytest.param(
+            "robust",
+            UNIFORM_DISTRIBUTION_DATA,
+            {"median": 10.0, "iqr": 10.0},
+            id="robust"
+        ),
 ])
-def test_normalization_parameters(client_on_dispatcher: Client, normalization_method: str):
+def test_normalization_parameters(
+        client_on_dispatcher: Client,
+        normalization_method: str,
+        data: dict,
+        expected: dict
+):
+    logger.info(data)
     client = client_on_dispatcher
     test_task = deepcopy(task_body)
     test_task['client'] = client.broker.queue
     test_task['module'] = 'findus_edge.algo.normalization'
     test_task['function'] = 'normalization'
     args = {
-        "input_data": MINMAX_DATA,
+        "input_data": data,
         "norm_method": normalization_method
     }
     test_task['arguments'] = args
@@ -38,3 +67,4 @@ def test_normalization_parameters(client_on_dispatcher: Client, normalization_me
     _, result = next(client.broker.pull())
     result = json.loads(result['result'])
     logger.info(json.dumps(result, indent=4))
+    assert result['parameters'] == expected, f'Normalization parameters differs from expected'
